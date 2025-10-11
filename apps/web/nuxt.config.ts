4f84ff9b6a78ccabdd3622d20e939d4b688caea9
wanghao <wanghao@ninghao.net>
2025-10-11 08:14:20 +0800

    feat(ui): enhance Nuxt configuration with Tailwind CSS integration and update dependencies; add global CSS file for Tailwind

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+/**
+ * Nuxt configuration
+ *
+ * This configuration file sets up the Nuxt application with the necessary
+ * configurations for development and production environments.
+ *
+ * @see https://nuxt.com/docs/api/nuxt-config
+ *
+ */
 export default defineNuxtConfig({
+  /**
+   * Set compatibility date for Nuxt
+   */
   compatibilityDate: '2025-07-15',
+
+  /**
+   * Nuxt devtools for development
+   */
   devtools: { enabled: true },
+
+  /**
+   * Nuxt Layers
+   */
   extends: ['@iniso/ui'],
 });
--- packages/ui/nuxt.config.ts
+++ packages/ui/nuxt.config.ts
@@ -1,5 +1,35 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
+/**
+ * Nuxt configuration
+ *
+ * This configuration file sets up the Nuxt application with the necessary
+ * configurations for development and production environments.
+ *
+ * @see https://nuxt.com/docs/api/nuxt-config
+ *
+ */
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  /**
+   * Set compatibility date for Nuxt
+   */
+  compatibilityDate: '2025-07-15',
+
+  /**
+   * Nuxt devtools for development
+   */
   devtools: { enabled: true },
+
+  /**
+   * CSS files to include globally
+   */
+  css: ['~/assets/css/tailwind.css'],
+
+  /**
+   * Vite plugins
+   */
+  vite: {
+    // Tailwind CSS
+    plugins: [tailwindcss()],
+  },
 });
