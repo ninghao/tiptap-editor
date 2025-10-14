cb5b424fab4dab9f4d09e4708f0d405c1a48df23
wanghao <wanghao@ninghao.net>
2025-10-14 09:32:04 +0800

    feat(vscode): add initial Nuxt configuration and project structure in .vscode/blank; include package.json, tsconfig.json, and app.vue for basic setup

---
+++ .vscode/blank/app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>@shu/blank</h1>
+  </div>
+</template>
---
+++ .vscode/blank/nuxt.config.ts
@@ -0,0 +1,20 @@
+/**
+ * Nuxt configuration
+ *
+ * This configuration file sets up the Nuxt application with the necessary
+ * configurations for development and production environments.
+ *
+ * @see https://nuxt.com/docs/api/nuxt-config
+ *
+ */
+export default defineNuxtConfig({
+  /**
+   * Set compatibility date for Nuxt
+   */
+  compatibilityDate: '2025-07-15',
+
+  /**
+   * Nuxt devtools for development
+   */
+  devtools: { enabled: true },
+});
